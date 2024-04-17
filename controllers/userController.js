44bb7dd89eeb9947f8eeacd35f7cb08e74cfede4
AdachiGayming <benjaminspeyerlarsen@gmail.com>
2024-04-17 13:56:06 +0200

    Axios

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,5 @@
+const axios = require('axios');
+const backend = "http://localhost:8888";
 
 let prompts = [];
 exports.user_chat_get = function(req, res) {
@@ -7,9 +9,20 @@ exports.user_chat_get = function(req, res) {
 
 exports.user_chat_post = function(req, res) {
     let { prompt:prompt } = req.body;
-    if (prompt != ''){
-        prompts.push(prompt);
-    res.render("chat", {title: "Chat", prompts})
-    }
+    
+    prompts.push(prompt)
+    axios.post(`${backend}/prompt/generate`, {data: prompt}, 
+        {headers: 
+            {'Content-Type': 'application/json'
+        }
+
+    }).then(response => {
+        prompts.push(response.data)
+        res.render("chat", {title: "Chat", prompts: prompts})
+
+    }).catch(function (error) {
+        console.log(error);
+
+    });
     
 }
