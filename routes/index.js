4a0b278eda529b1ab693381933d3d4d8879c771f
GhostMG14 <OD28KM@student.aau.dk>
2024-05-03 01:56:27 +0200

    fix to merge
    fix to merge

--- routes/index.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-const index_controller = require("../controllers/indexController");
-
-/* GET home page. */
-router.get('/', index_controller.homepage_get)
-
-module.exports = router;
