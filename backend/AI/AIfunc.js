1837ddc0343ffbc01b0ca5ca8ef33900a7fddc8a
MGLarsenSW <mathias.larsen9@gmail.com>
2024-05-23 11:59:53 +0200

    Stable version

--- backend/AI/AIfunc.js
+++ backend/AI/AIfunc.js
@@ -1,41 +1,30 @@
-const PyPath = require('./PyPath');
-const {addAnswerAndSend} = require("../../frontend/controllers/chatController");
-
-let resGlobal;
-
-exports.askAI = (res, question, selectedDataset) => {
+const getPythonPath = require('./PyPath');
+exports.askAI = (res, question, selectedDataset, callback) => {
 
 
     console.log("AskAI called...")
-    resGlobal = res;
+    const spawn = require("child_process").spawn;
 
-    sendDataToPython(question + "\n" + selectedDataset)
+    //Path to your python exe
 
-    console.log("AskAI done...")
+    const pythonExePath = "C:/Users/Mathias/AppData/Local/Programs/Python/Python312/python.exe";
 
+    //const pythonExePath = getPythonPath();
+    //Path to python script, Might need absolut path
+    const AIPath = "./../../backend/AI/AIScript.py";
+
+    const ask = spawn(pythonExePath, [AIPath, question, selectedDataset]);
+
+    let ans;
+
+    ask.stdout.on('data', (data) => {
+        console.log(data.toString()); // Shows the answer in the terminal
+        console.log("AskAI done...")
+        ans = data.toString();
+        callback(res, ans);
+    })
+    // Handle error output from the Python script
+    ask.stderr.on('data', (data) => {
+        console.error(`Error from Python: ${data}`);
+    });
 }
-const spawn = require("child_process").spawn;
-//Path to your python exe
-
-//const pythonExePath = "C:/Users/stefa/AppData/Local/Microsoft/WindowsApps/python.exe"
-const pythonExePath = "C:/Users/stefa/AppData/Local/Programs/Python/Python312/python.exe";
-//Path to python script, Might need absolut path
-//const pythonExePath = getPythonPath();
-const AIPath = "./../../backend/AI/AIScript.py";
-
-const ask = spawn(pythonExePath, [AIPath]);
-ask.stdout.on('data', (data) => {
-    console.log(data.toString()); // Shows the answer in the terminal
-
-    let ans = data.toString();
-    addAnswerAndSend(resGlobal, ans);
-})
-// Handle error output from the Python script
-ask.stderr.on('data', (data) => {
-    console.error(`Error from Python: ${data}`);
-});
-function sendDataToPython(data) {
-    ask.stdin.write(`${data}\n`);
-}
-
-
